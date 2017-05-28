faf3020ac0daa2c6e9de9ec1fa2aed7f7c9c1702
Jovs <jvlquiamco@gmail.com>
2017-05-28 20:21:43 +0800

    Marker update

--- app/src/main/java/com/example/avinnovz/whernau/activities/MainActivity.java
+++ app/src/main/java/com/example/avinnovz/whernau/activities/MainActivity.java
@@ -1,10 +1,23 @@
 package com.example.avinnovz.whernau.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.widget.Button;
 
 import com.example.avinnovz.whernau.R;
 import com.example.avinnovz.whernau.base.BaseActivity;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -15,15 +28,16 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
-public class MainActivity extends BaseActivity implements OnMapReadyCallback{
+public class MainActivity extends BaseActivity implements OnMapReadyCallback, ConnectionCallbacks,
+        OnConnectionFailedListener, LocationListener{
 
     @BindView(R.id.btn_change_marker)
     Button btn_change_marker;
 
     private GoogleMap map;
     private Marker marker;
+    private GoogleApiClient googleApiClient;
     private boolean isFirst;
 
     @Override
@@ -32,6 +46,7 @@ public class MainActivity extends BaseActivity implements OnMapReadyCallback{
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         initMap();
+        buildGoogleApiClient();
     }
 
     private void initMap() {
@@ -46,7 +61,64 @@ public class MainActivity extends BaseActivity implements OnMapReadyCallback{
             map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
             map.getUiSettings().setScrollGesturesEnabled(true);
             map.getUiSettings().setZoomGesturesEnabled(true);
-            LatLng latLng = new LatLng(14.695179, 121.117852);
+        }
+    }
+
+    private void updatedLocation(LatLng latLng) {
+        marker.setPosition(latLng);
+        map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (googleApiClient != null) {
+            googleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (googleApiClient != null) {
+            googleApiClient.disconnect();
+        }
+    }
+
+    synchronized void buildGoogleApiClient() {
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        if (googleApiClient != null) {
+            googleApiClient.connect();
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(3000); // update location every 3 seconds
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+        Location location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        if (location != null) {
+            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
             MarkerOptions markerOptions = new MarkerOptions()
                     .position(latLng);
             marker = map.addMarker(markerOptions);
@@ -54,12 +126,18 @@ public class MainActivity extends BaseActivity implements OnMapReadyCallback{
         }
     }
 
-    @OnClick(R.id.btn_change_marker)
-    public void changeMarkerLocation() {
-        LatLng latLng = isFirst ? new LatLng(14.695179, 121.117852) :
-                new LatLng(14.7011, 120.9830);
-        isFirst = !isFirst;
-        marker.setPosition(latLng);
-        map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        buildGoogleApiClient();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        updatedLocation(new LatLng(location.getLatitude(), location.getLongitude()));
     }
 }
